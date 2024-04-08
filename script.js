74cbd50fd894c8af66430b7b2d0c0c9dafa516c6
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2024-04-08 11:58:14 +0100

    Tabbed Component

--- script.js
+++ script.js
@@ -64,6 +64,32 @@ navToggle.addEventListener('click', function() {
 
 
 
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons)
+
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if(!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  clicked.classList.add('operations__tab--active');
+
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+}
+);
 
 
 
